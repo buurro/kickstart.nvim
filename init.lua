b96be3b807126da57210deada726b09a9ac14247
Marco Burro <marcoburro98@gmail.com>
2024-05-03 19:40:05 +0200

    nix fmt

--- init.lua
+++ init.lua
@@ -764,12 +764,16 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        nix = { 'nixpkgs_fmt' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
         php = { { 'prettierd', 'prettier' } },
+      },
+      formatters = {
+        nixpkgs_fmt = { command = 'nixpkgs-fmt' },
       },
     },
   },
