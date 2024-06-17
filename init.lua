b15b2ce6afe5e2563b83be22b53f23c1d8bf746d
Marco Burro <marcoburro98@gmail.com>
2024-06-17 08:30:49 +0200

    tab lines

--- init.lua
+++ init.lua
@@ -330,6 +330,8 @@ require('lazy').setup({
     },
   },
 
+  { 'lukas-reineke/indent-blankline.nvim', main = 'ibl', opts = {} },
+
   -- NOTE: Plugins can specify dependencies.
   --
   -- The dependencies are proper plugin specifications as well - anything
