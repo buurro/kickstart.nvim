b41fd8a43775f23a39f2c3a20ceddf3ca3c30bca
Marco Burro <marcoburro98@gmail.com>
2024-06-21 12:23:53 +0200

    fix indent blankline

--- init.lua
+++ init.lua
@@ -336,8 +336,6 @@ require('lazy').setup({
     },
   },
 
-  { 'lukas-reineke/indent-blankline.nvim', main = 'ibl', opts = {} },
-
   -- NOTE: Plugins can specify dependencies.
   --
   -- The dependencies are proper plugin specifications as well - anything
@@ -973,7 +971,7 @@ require('lazy').setup({
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
