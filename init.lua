36976c47d574e86fd30e311b3143583115dc3f4f
Marco Burro <marcoburro98@gmail.com>
2025-02-13 10:46:09 +0100

    fix php formatter

--- init.lua
+++ init.lua
@@ -794,7 +794,7 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
-        php = { { 'prettierd', 'prettier' } },
+        php = { 'prettier' },
       },
       formatters = {
         nixpkgs_fmt = { command = 'nixpkgs-fmt' },
